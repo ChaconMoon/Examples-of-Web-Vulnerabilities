b119fb5a3ffde358b224bbbc2d0656fd9786b5ee
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-04-10 17:54:08 +0200

    Update unsafe deserialization

--- UNSAFE_DESERIALIZATION/deserialize.php
+++ UNSAFE_DESERIALIZATION/deserialize.php
@@ -1,13 +1,10 @@
 <?php
 class User {
-    public $username = "hacker";
-    public $isAdmin = true;
+    public $username;
+    public $isAdmin = false;
 }
-if (isset($_GET['data'])) {
-    $data = unserialize($_GET['data']);
-if ($data->$isAdmin) {
-    echo "¡Acceso de administador concedido!"
+$data = unserialize($_GET['data']);
+if ($data->isAdmin) {
+    echo "¡Acceso de administrador concedido!"
 }
-}
-echo urldecode(serialize(new User()))
 ?>
