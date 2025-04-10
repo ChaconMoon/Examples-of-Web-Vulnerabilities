0a90941b5dbf3aa35d2ec5fe912bbd0973a2f650
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-04-10 19:36:22 +0200

    Create UnSafe Deserialization JSON

---
+++ UNSAFE_DESERIALIZATION/deserialize_json.php
@@ -0,0 +1,21 @@
+<?php
+class User {
+    public $username;
+    public $isAdmin = false;
+}
+
+$json = $_GET['data'] ?? "{}" // Si no se pasa nada como paraemtro data lo interpetara como un fichero
+
+$data = json_decode($json, true);
+
+
+if (!is_array($data)) {
+    die("Formato de datos no valido")
+}
+
+if (isset ($data ["isAdmin"]) & $data("isAdmin") === true) {
+    echo "Acceso de administrador concedido"
+} else {
+    echo "Acceso normal"
+}
+?>
