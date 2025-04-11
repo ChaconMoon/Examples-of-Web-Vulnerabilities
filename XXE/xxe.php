0b946b7458958dd5a842f634281e1237efc4cd57
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-04-11 11:48:47 +0200

    Creare XXE Vulnerability

---
+++ XXE/xxe.php
@@ -0,0 +1,14 @@
+<?php
+
+# Crear un objeto DOMDocument
+$dom = new DOMDocument();
+
+# Habilitar cargas de contenido externas (solo para pruebas)
+$dom = loadXML(file_get_contents('php://input'), LIBXML_NOENT | LIBXML_DTDLOAD);
+
+# Convertir el XML a un objeto SimpleXMLElement (opcional)
+$parsed = socket_import_dom($dom);
+
+# Mostrar el resultado
+echo $parsed
+?>
