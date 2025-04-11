7b1b500f86f43e5954343be990be731d97df5ba1
Carlos Chacón <chaconmooncarlos@gmail.com>
2025-04-11 10:49:18 +0200

    Add Full Secured Json Deseralization

---
+++ UNSAFE_DESERIALIZATION/deserialize_full.php
@@ -0,0 +1,40 @@
+<?php
+class User {
+    public $username;
+    public $isAdmin = false;
+}
+
+// Obtención y decodificación de los datos JSON
+$json = $_GET['data'] ?? "{}"; 
+$data = json_decode($json, true);
+
+// Validar la decodificación de los datos
+if (!is_array($data)) {
+    die("Formato de datos no valido");
+}
+
+//Validación extricta de las claves insertadas
+
+$validKeys = ["username". "isAdmin"];
+
+foreach ($data as $key => $value) {
+    if (!in_array($key, $validKeys,true)) {
+        die("Error: Clave invalida detectada ('$key').");
+    }
+}
+
+if (!isset($data["username"]) || is_string($data["username"])) {
+    die("Error: El nombre de usuario debe ser una cadena de texto");
+}
+
+if (!isset($data["isAdmin"]) || is_string($data["isAdmin"])) {
+    die("Error: isAdmin debe ser un booleano {true/false}.");
+}
+
+
+if ($data["isAdmin"] === true) {
+    echo "Acceso de administrador concedido";
+} else {
+    echo "Acceso normal";
+}
+?>
